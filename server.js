f08ac0833bd9e80892f4b2484c65a4b3292b1e55
Ian <0306pitt@gmail.com>
2022-11-30 20:40:33 -0500

    question layout in questions.js

--- server.js
+++ server.js
@@ -2,3 +2,23 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 
+
+
+// New Department
+function newDepartment() {
+    inquirer
+    .prompt()
+    .then((response) => {
+        db.query(add, response.name, function (err, results) {
+            console.log(`New department ${response.name} was added!`);
+            init();
+        });
+    });
+};
+
+// New Role
+
+
+
+
+// Questions
