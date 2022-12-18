f4820fdc661a8215bec9e1c7b6a75b324a73d8df
Ian <0306pitt@gmail.com>
2022-12-18 00:49:57 -0500

    add ons

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,28 @@
 
-DROP DATABASE
+DROP DATABASE IF Exists tracking_db;
+CREATE DATABASE tracking_db;
+
+USE tracking_db;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT
+);
+
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    firstName VARCHAR(30) NOT NULL,
+    lastName VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT
+);
+
